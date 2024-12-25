2f88a880032bbaacf8c74d352d38563af1c703f2
이승연 <dltmddus9082@naver.com>
2024-12-26 00:54:52 +0900

    refactor: SpringBootTest 어노테이션 삭제 (#9)

--- src/test/java/umc/spring/ApplicationTests.java
+++ src/test/java/umc/spring/ApplicationTests.java
@@ -3,7 +3,7 @@ package umc.spring;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class ApplicationTests {
 
 	@Test
